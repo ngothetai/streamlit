cdf8785f4844e3b40b7fe28442d053d686e8df9b
thetai <thetai2382002@gmail.com>
2023-09-24 19:07:59 +0700

    hello

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.header("Hello moi nguoi!")
-num = st.text_input("Nhập một con số bạn yêu thích")
+num = st.number_input("Nhập một con số bạn yêu thích")
 don_vi_can_nang = st.radio("chọn đơn vị chiều cao", options=["a","b","c"])
 def calculate():
     st.success("Số yêu thích của bạn là số " + "chẵn" if num%2==0 else "lẻ!" + str(don_vi_can_nang))
